232f7adc03b6b694291e60241bf5ee61fdc2051d
jessyy2006 <worldlive145@gmail.com>
2025-06-17 11:39:59 +0200

    fixed? assumed that error has to do w camera not being ready yet, so waiting for loadedmetadata event

--- script.js
+++ script.js
@@ -92,17 +92,21 @@ function zoomSetUp() {
   }
 
   // check if the camera has zoom capabilities (same cam for videoZoom and videoFull so just check 1)
-  let capabilities = videoZoom.srcObject.getVideoTracks()[0].getCapabilities();
-  console.log(capabilities);
+  videoZoom.addEventListener("loadedmetadata", () => {
+    let capabilities = videoZoom.srcObject
+      .getVideoTracks()[0]
+      .getCapabilities();
+    console.log(capabilities);
 
-  if ("zoom" in capabilities) {
-    let min = capabilities["zoom"]["min"]; // get the min and max zoom values embedded in cam
-    let max = capabilities["zoom"]["max"];
-    console.log("min: " + min);
-    console.log("max: " + max);
-  } else {
-    alert("This camera does not support zoom");
-  }
+    if ("zoom" in capabilities) {
+      let min = capabilities["zoom"]["min"]; // get the min and max zoom values embedded in cam
+      let max = capabilities["zoom"]["max"];
+      console.log("min: " + min);
+      console.log("max: " + max);
+    } else {
+      alert("This camera does not support zoom");
+    }
+  });
 }
 zoomSetUp();
 
